20ed01040ccc606cf77020f2ce9abdaaf70170c9
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-05-27 11:59:26 +0200

    Update view.php

---
+++ assets/core/view.php
@@ -0,0 +1,307 @@
+<?php
+$array = [
+    "category" => [
+        [
+            "category" => "A",
+            "chapter" => "Algemene bepalingen verkeerswetgeving"
+        ],
+        [
+            "category" => "B",
+            "chapter" => "Rijbevoegdheid en rijbewijzen"
+        ],
+        [
+            "category" => "C",
+            "chapter" => "Inrichting, belading en slepen van voertuigen"
+        ],
+        [
+            "category" => "D",
+            "chapter" => "Techniek, onderhoud en controle van voertuigen"
+        ],
+        [
+            "category" => "E",
+            "chapter" => "Gebruik gordels en helmen; zitplaats voor passagiers"
+        ],
+        [
+            "category" => "F",
+            "chapter" => "Milieubewust en energiezuinig rijden"
+        ],
+        [
+            "category" => "G",
+            "chapter" => "Risico\u2019s in verband met toestand bestuurder"
+        ],
+        [
+            "category" => "H",
+            "chapter" => "Risico\u2019s in verband met eigenschappen en toestand voertuig"
+        ],
+        [
+            "category" => "I",
+            "chapter" => "Verkeerssituaties"
+        ],
+        [
+            "category" => "J",
+            "chapter" => "Risico\u2019s in verband met aanwezigheid en gedrag ander verkeer"
+        ],
+        [
+            "category" => "K",
+            "chapter" => "Risico's in verband met weg-, zicht- en weersomstandigheden"
+        ],
+        [
+            "category" => "L",
+            "chapter" => "Handelen bij ongevallen en pech onderweg"
+        ],
+        [
+            "category" => "M",
+            "chapter" => "Voor laten gaan op kruispunten [voorrang]"
+        ],
+        [
+            "category" => "N",
+            "chapter" => "Voor laten gaan bij het afslaan"
+        ],
+        [
+            "category" => "O",
+            "chapter" => "Voor laten gaan van blinden, gehandicapten en voetgangers"
+        ],
+        [
+            "category" => "P",
+            "chapter" => "Voor laten gaan van voorrangsvoertuigen, mil. colonnes en trams"
+        ],
+        [
+            "category" => "Q",
+            "chapter" => "Uitvoeren van \/ voor laten gaan bij bijzondere manoeuvres"
+        ],
+        [
+            "category" => "R",
+            "chapter" => "Plaats op de weg en voorsorteren"
+        ],
+        [
+            "category" => "S",
+            "chapter" => "Inhalen"
+        ],
+        [
+            "category" => "T",
+            "chapter" => "Snelheid"
+        ],
+        [
+            "category" => "U",
+            "chapter" => "Stilstaan en parkeren"
+        ],
+        [
+            "category" => "V",
+            "chapter" => "Geven van tekens en signalen"
+        ],
+        [
+            "category" => "W",
+            "chapter" => "Gebruik van lichten"
+        ],
+        [
+            "category" => "X",
+            "chapter" => "Verkeersborden"
+        ],
+        [
+            "category" => "Y",
+            "chapter" => "Verkeerslichten en aanwijzingen"
+        ],
+        [
+            "category" => "Z",
+            "chapter" => "Verkeerstekens op het wegdek"
+        ]
+    ],
+    "vragen" => [
+        {
+            "id": "1182",
+            "category": "T",
+            "type": "1",
+            "image": "BB07R-A16-52,8.png",
+            "question": "Wat geeft dit bord aan?",
+            "feedback": "Door het ronde bordje met de rode rand boven het hectometer bord zie je dat dit de maximumsnelheid aangeeft voor deze weg. ",
+            "options": [
+                "Maximumsnelheid",
+                "Minimumsnelheid",
+                "Adviessnelheid"
+            ]
+        },
+        {
+            "id": "1152",
+            "category": "T",
+            "type": "1",
+            "image": "a01-50_h1.png",
+            "question": "Welk bord geeft de bebouwde kom aan? ",
+            "feedback": "Bord B: ‘Amsterdam’ geeft aan dat je de bebouwde kom inrijdt. Bord A geeft een maximumsnelheid aan. ",
+            "options": [
+                "Bord B",
+                "Bord A",
+                ""
+            ]
+        },
+        {
+            "id": "1178",
+            "category": "T",
+            "type": "1",
+            "image": "395.jpg",
+            "question": "Je rijdt 80 km/u. Wat is het meest verstandig om te doen?",
+            "feedback": "Je rijd te dicht op de vrachtwagen. Hierdoor moet je je afstand tussen jou en de vrachtwagen vergroten. ",
+            "options": [
+                "Vergroot de afstand tussen jou en de vrachtwagen",
+                "Verklein de afstand tussen jou en de vrachtwagen",
+                ""
+            ]
+        },
+        {
+            "id": "1167",
+            "category": "T",
+            "type": "1",
+            "image": "389a.jpg",
+            "question": "De maximumsnelheid op deze weg is…..? ",
+            "feedback": "Ondanks dat dit een voorrangsweg is, rijd je binnen de bebouwde kom. Hier mag je dus maar 50km/u. ",
+            "options": [
+                "50 km/u",
+                "60 km/u",
+                "80 km/u"
+            ]
+        },
+        {
+            "id": "1200",
+            "category": "T",
+            "type": "1",
+            "image": "390.jpg",
+            "question": "Wat is de maximumsnelheid voor een auto met een aanhanger op de autoweg? \r\n",
+            "feedback": "De maximumsnelheid voor een auto met een aanhanger op de autoweg is 90 km/u.",
+            "options": [
+                "90 km/u",
+                "100 km/u",
+                "120 km/u"
+            ]
+        },
+        {
+            "id": "1164",
+            "category": "T",
+            "type": "1",
+            "image": "A02-80.png",
+            "question": "Wat betekent dit bord? ",
+            "feedback": "Dit bord betekend einde maximumsnelheid. ",
+            "options": [
+                "Einde maximumsnelheid",
+                "Einde alle verboden",
+                "Einde van adviessnelheid"
+            ]
+        },
+        {
+            "id": "1187",
+            "category": "T",
+            "type": "1",
+            "image": "a03-80_A04-80.png",
+            "question": "Welk bord geeft een maximumsnelheid aan?",
+            "feedback": "Het matrixbord geeft de maximumsnelheid aan. Het andere bord is een adviessnelheid. ",
+            "options": [
+                "Bord A",
+                "Bord B",
+                ""
+            ]
+        },
+        {
+            "id": "1163",
+            "category": "T",
+            "type": "1",
+            "image": "043b.jpg",
+            "question": "De maximumsnelheid op deze weg is op dit moment...?",
+            "feedback": "Bij een aangepaste snelheid op een matrixbord moet je altijd de aangegeven snelheid aanhouden. ",
+            "options": [
+                "70km/u",
+                "120km/u",
+                "100km/u"
+            ]
+        },
+        {
+            "id": "1159",
+            "category": "T",
+            "type": "1",
+            "image": "B01.png",
+            "question": "Dit bord is geplaatst na de kruising. Waar ben je nu?\r\n",
+            "feedback": "Is het bord ‘voorrangsweg’ geplaatst na een kruising, dan bevind je je buiten de bebouwde kom. ",
+            "options": [
+                "Buiten de bebouwde kom",
+                "Binnen de bebouwde kom",
+                ""
+            ]
+        },
+        {
+            "id": "1155",
+            "category": "T",
+            "type": "2",
+            "image": "030.jpg",
+            "question": "Moet je een gordel dragen in een T100 bus?",
+            "feedback": "In een T100 bus is het verplicht dat er gordels aanwezig zijn op elke zitplaats. Als deze aanwezig zijn, ben je ook verplicht deze om te doen. ",
+            "options": [
+                "JA",
+                "NEE",
+                ""
+            ]
+        },
+        {
+            "id": "1165",
+            "category": "T",
+            "type": "1",
+            "image": "388.jpg",
+            "question": "De maximumsnelheid op deze weg is…..?",
+            "feedback": "Dit bord geeft aan dat je de bebouwde kom ingaat. Hier mag je dus 50km/u.",
+            "options": [
+                "50 km/u",
+                "60 km/u",
+                "80 km/u"
+            ]
+        },
+        {
+            "id": "1201",
+            "category": "T",
+            "type": "1",
+            "image": "181.png",
+            "question": "Wat gebeurt er met de middelpuntvliedende kracht (centrifugale kracht) wanneer je jouw snelheid verdubbelt?\r\n",
+            "feedback": "De centrifugale kracht verviervoudigt wanneer je je snelheid verdubbelt. ",
+            "options": [
+                "De kracht verviervoudigt",
+                "De kracht verdubbelt",
+                "De kracht verdriedubbelt"
+            ]
+        },
+        {
+            "id": "1149",
+            "category": "T",
+            "type": "1",
+            "image": "033.png",
+            "question": "Welke informatie kun je vinden op een hectometerpaaltje?\r\n",
+            "feedback": "Op een hectometerpaaltje kun je het routenummer, het deel van de weg, de kilometeraanduiding en de maximumsnelheid vinden. De maximumsnelheid wordt soms echter achterwege gelaten.\r\n",
+            "options": [
+                "Routenummer, wegdeel, kilometeraanduiding, maximumsnelheid.",
+                "Routenummer, kilometeraanduiding, maximumsnelheid.",
+                "Routenummer, maximumsnelheid."
+            ]
+        },
+        {
+            "id": "1151",
+            "category": "T",
+            "type": "1",
+            "image": "301.jpg",
+            "question": "Hoe hard mag een bestelauto op de snelweg rijden met een aanhangwagen (totale gewicht 3000kg)?",
+            "feedback": "De maximumsnelheid van een auto met aanhangwagen op de snelweg is 90km/u. ",
+            "options": [
+                "90 km/u",
+                "80 km/u",
+                "50 km/u"
+            ]
+        },
+        {
+            "id": "1206",
+            "category": "T",
+            "type": "1",
+            "image": "185.png",
+            "question": "Hoe hard mag een gehandicaptenvoertuig (met motor) rijden op het voetpad?\r\n",
+            "feedback": "Met een gehandicaptenvoertuig, met of zonder motor, op een voetpad mag u maximaal 6 km/u.",
+            "options": [
+                "6 km/u",
+                "10 km/u",
+                "25 km/u"
+            ]
+        },
+    ]
+]
+    ?>
