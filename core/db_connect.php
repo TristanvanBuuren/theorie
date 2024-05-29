f351e104cd2e6d9e9913d0858a9ad51d991ecfbd
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-05-29 12:26:51 +0200

    making it responsive

--- core/db_connect.php
+++ core/db_connect.php
@@ -6,20 +6,20 @@ session_start();
  * Voor de Windows gebruikers;
  */
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "theorie";
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "";
+// $dbname = "theorie";
 
-$con = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+// $con = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-if ($con -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $con -> connect_error;
-    exit();
- }
+// if ($con -> connect_errno) {
+//     echo "Failed to connect to MySQL: " . $con -> connect_error;
+//     exit();
+//  }
 
-define("BASEURL","http://localhost/module2/theorie/");
-// define("BASEURL","http://localhost/theorie/");
+// define("BASEURL","http://localhost/module2/theorie/");
+define("BASEURL","http://localhost/theorie/");
 
 function prettyDump ( $var ) {
     echo "<pre>";
