b899774500d8b8c1328889424158664ef659f40b
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-05-29 11:31:59 +0200

    header

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 // include("core/connect_db.php");
-include("core/header.php");
+include ("core/header.php");
 ?>
 <main>
-<h1 class="inline-flex" id="title">CBR oefenexamen</h1>
-<img class="inline-flex" id="logo" src="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f698.svg" alt="LOGO"/>
-<h2 id="choose">Kies een categorie</h2>
+    <div class="flex">
+        <h1 class="flex" id="title">CBR oefenexamen</h1>
+        <img class="flex" id="logo" src="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f698.svg"
+            alt="LOGO" />
+    </div>
+
+    <h2 id="choose">Kies een categorie</h2>
 
 </main>
 <?php
-include("core/footer.php");
+include ("core/footer.php");
 ?>
