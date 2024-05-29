5d4cfef55330b934274d8f4256c9811f84249ec2
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-05-29 11:59:29 +0200

    loading in categories

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 // include("core/connect_db.php");
 include ("core/header.php");
+
+
 ?>
 <main>
     <div class="flex" id="header">
@@ -9,13 +11,24 @@ include ("core/header.php");
             alt="LOGO" />
     </div>
     <div id="choose">Kies een categorie</div>
-    <div id="content">
-        <div class="category" id="category-b">
+    <div class="content">
+        <?php
+        include ("core/view.php");
+        foreach ($array["category"] as $category) {
+            ?>
+            <div class="category" id="category-<?= $category["category"];?>">
+                <div><?= $category["category"];?></div>
+                <div class="cat-title"><?= $category["chapter"];?></div>
+            </div>
+            <?php
+        }
+        ?>
+        <!-- <div class="category" id="category-b">
             <div id="cat-b">B</div>
             <div class="cat-title">Rijbevoegdheid en rijbewijzen</div>
         </div>
 
-        <div class="category" id="cat-t">T</div>
+        <div class="category" id="cat-t">T</div> -->
     </div>
 
 </main>
