7d3ffd5b3938acc79c01f2eb8ef4f4ccc6d98f99
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-05-29 11:35:15 +0200

    Page

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
 include ("core/header.php");
 ?>
 <main>
-    <div class="flex">
+    <div class="flex" id="header">
         <h1 class="flex" id="title">CBR oefenexamen</h1>
         <img class="flex" id="logo" src="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f698.svg"
             alt="LOGO" />
     </div>
-
     <h2 id="choose">Kies een categorie</h2>
+    <div id="content">
+        <div class="category" id="cat-b">B</div>
+        <div class="category" id="cat-t">T</div>
+    </div>
 
 </main>
 <?php
