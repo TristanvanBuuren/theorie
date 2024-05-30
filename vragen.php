ec06c9714fa9c69f106286b20611f8f1229a9751
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-05-30 09:56:47 +0200

    Update vragen.php

--- vragen.php
+++ vragen.php
@@ -6,7 +6,29 @@
 <div class="blauwe-balkh"></div>
 
 <div class="blauwe-balkl"></div>
+<?php
+$cat1 = $_GET['cat'];
 
+$sqli_prepare = $con->prepare("SELECT vragen_id, vragen_category_id, type, image, question, feedback, option_1, option_2, option_3 FROM vragen WHERE vragen_category_id = $cat1;");
+if ($sqli_prepare === false) {
+  echo mysqli_error($con);
+} else {
+  if ($sqli_prepare->execute()) {
+    $sqli_prepare->store_result();
+    $sqli_prepare->bind_result($vragen_id, $vragen_category_id, $type, $image, $question, $feedback, $option_1, $option_2, $option_3);
+    while ($sqli_prepare->fetch()) { // WHILE START
+      
+      $ids[] = $vragen_id;
+      
+      ?>
+        
+      <?php
+    }
+    print_r($ids);
+  }
+}
+$sqli_prepare->close();
+?>
 <div class="container">
     <div class="row">
         <div class="col-md-8 offset-md-2">
