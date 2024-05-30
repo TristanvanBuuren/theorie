c51e091c1e3c64f5208a3849ee30ec66f8dcc9a7
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2024-05-30 08:56:40 +0200

    vragen.php erbij

---
+++ vragen.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Blauwe Balk</title>
+<style>
+ 
+</style>
+</head>
+<body>
+
+<div class="blauwe-balk"></div>
+
+</body>
+</html>
