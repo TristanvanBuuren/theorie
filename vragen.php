9d934e42785e377a8ae59522b126081ffa566511
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2024-05-30 09:44:47 +0200

    vragen.php

--- vragen.php
+++ vragen.php
@@ -3,7 +3,47 @@
   include('core/header2.php');
 ?>
 
-<div class="blauwe-balk"></div>
+<div class="blauwe-balkh"></div>
+<div class="blauwe-balkl"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="vraag-balk">
+                
+                <p>Wat is de hoofdstad van Nederland?</p>
+            </div>
+            <div class="antwoord-box" onclick="selectAntwoord(this)">
+                Amsterdam
+            </div>
+            <div class="antwoord-box" onclick="selectAntwoord(this)">
+                Rotterdam
+            </div>
+            <div class="antwoord-box" onclick="selectAntwoord(this)">
+                Den Haag
+            </div>
+            <button class="verder-knop" onclick="verderGaan()">Verder</button>
+        </div>
+        </div>
+    </div>
+</div>
+<script>
+    function selectAntwoord(element) {
+        // Verwijder eerst 'selected' klasse van alle antwoordboxen
+        var antwoordBoxen = document.querySelectorAll('.antwoord-box');
+        antwoordBoxen.forEach(function(box) {
+            box.classList.remove('selected');
+        });
+        // Voeg 'selected' klasse toe aan de geselecteerde antwoordbox
+        element.classList.add('selected');
+    }
 
-</body>
-</html>
+    function verderGaan() {
+        var geselecteerdeAntwoord = document.querySelector('.antwoord-box.selected').innerText;
+        alert("Geselecteerd antwoord: " + geselecteerdeAntwoord);
+        window.location.href = "volgende.php";
+    }
+</script>
+
+<?php
+  include('core/footer.php');
+?>
