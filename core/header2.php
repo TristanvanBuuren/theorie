489a66738e4105af0647b98a0300923e336b6f4a
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2024-05-30 09:06:47 +0200

    vragen change

--- core/header2.php
+++ core/header2.php
@@ -1,8 +1,7 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT'] . '/Module2/theorie/core/db_connect.php');
-
+//   include($_SERVER['DOCUMENT_ROOT'] . '/Module2/theorie/core/db_connect.php');
+  include('db_connect.php');
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,8 +9,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/Module2/theorie/core/db_connect.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= BASEURL;?>assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?= BASEURL;?>assets/css/login.css">
-    
+    <link rel="stylesheet" href="<?= BASEURL;?>assets/css/style.css">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -21,19 +19,17 @@ include($_SERVER['DOCUMENT_ROOT'] . '/Module2/theorie/core/db_connect.php');
 <body>
 <nav>
         <ul>
-            <li><a href="../index.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <?php if (isset($_SESSION['ingelogd']) && $_SESSION['ingelogd'] === true): ?>
-                <li><a href="account.php">Account</a></li>
-                <li><a href="uitloggen.php">Uitloggen</a></li>
-
-                <?php elseif (isset($_SESSION['admin_ingelogd']) && $_SESSION['admin_ingelogd'] === true): ?>
-                    <li><a href="../login/admin/admin_account.php">Admin Account</a></li>
-                <li><a href="uitloggen.php">Uitloggen</a></li>
+                <li><a href="login/account.php">Account</a></li>
+                <li><a href="login/uitloggen.php">Uitloggen</a></li>
                 
+                <?php elseif (isset($_SESSION['admin_ingelogd']) && $_SESSION['admin_ingelogd'] === true): ?>
+                <li><a href="login/admin/admin_account.php">Admin Account</a></li>
+                <li><a href="login/uitloggen.php">Uitloggen</a></li>
+
             <?php else: ?>
-               
-                <li><a href="login.php">Inloggen</a></li>
+                <li><a href="login/login.php">Inloggen</a></li>
             <?php endif; ?>
         </ul>
     </nav>
-    
---
+++ vragen.php
@@ -0,0 +1,8 @@
+<?php
+  include('core/header2.php');
+?>
+
+<div class="blauwe-balk"></div>
+
+</body>
+</html>
