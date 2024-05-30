50c93f41c1e7e92c3ae67e13a27e3468a63ead16
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-05-30 09:27:51 +0200

    added the query

--- questions.php
+++ questions.php
@@ -1,22 +1,57 @@
 <?php
-// include("core/db_connect.php");
-include ("core/header.php");
+include("core/db_connect.php");
+// include ("core/header.php");
 // include("core/view.php");
 
 ?>
 <main>
-    <div>
-        <div id="time"></div>
-        <div id="category"></div>
-    </div>
+<?php
+$cat1 = $_GET['cat'];
 
-    SELECT * FROM `vragen` WHERE `vragen_category_id` = 2 AND `vragen_id` = [array];
+$sqli_prepare = $con->prepare("SELECT vragen_id, vragen_category_id, type, image, question, feedback, option_1, option_2, option_3 FROM vragen WHERE vragen_category_id = $cat1;");
+if ($sqli_prepare === false) {
+  echo mysqli_error($con);
+} else {
+  if ($sqli_prepare->execute()) {
+    $sqli_prepare->store_result();
+    $sqli_prepare->bind_result($vragen_id, $vragen_category_id, $type, $image, $question, $feedback, $option_1, $option_2, $option_3);
+    while ($sqli_prepare->fetch()) { // WHILE START
+      ?>
+      <div class='product'>
+        <a href='productpage.php?id=<?= $producten_id . "&cat=" . $category_id ?>'>
+        </a>
+        <span class='product-text'>
+          <a class='td-none' href='productpage.php?id=<?= $producten_id . "&cat=" . $category_id ?>'>
+            <?= $producten_naam ?>
+          </a>
+        </span>
+        <div class='cart-button' onclick='ManageStorage("<?= $producten_msName ?>")'>
+          <img class='cart-img' alt='cart' width='40' height='40' src='./assets/img/cart.png' />
+        </div>
+        <!-- <button type="sumbit" class='cart-button'>
+          <img class='cart-img' alt='cart' width='40' height='40' src='./assets/img/cart.png' />
+        </> -->
+        <span class='beschrijving'>
+          <?= $producten_tekst ?>
+        </span>
+        <span class='prijs'>
+          € <?= $producten_prijs ?>
+        </span>
+      </div>
+      <?php
+    }
+  }
+}
+$sqli_prepare->close();
+?>
+
+    <!-- SELECT * FROM `vragen` WHERE `vragen_category_id` = 2 AND `vragen_id` = [array];
 
 
     array = [621, 624, 626, 628, 629, 631, 632, 633, 634, 635, 636, 637, 638, 640, 641, 642, 643, 644, 645, 646];
 
     SELECT vragen_id FROM `vragen` ORDER BY vragen_id ASC;
-    and then put this into an array ^
+    and then put this into an array ^ -->
 
 </main>
 <?php
