66f709d7a45e094f6b934f99b013fac6d5e4f2cf
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-05-30 08:51:06 +0200

    working on fixing questions.php styling

--- questions.php
+++ questions.php
@@ -9,24 +9,7 @@ include ("core/header.php");
         <div id="time"></div>
         <div id="category"></div>
     </div>
-    <?php
-        // include ("core/view.php");
 
-        // $cat = $_GET["cat"];
-        // // echo $cat;
-
-        // foreach ($array["vragen"] as $questions) {
-        //     if ($questions["category"] == $cat) {
-        //     ?>
-        //     <!-- <div class="category td-none" id="category-<?= $questions["category"];?>" href="questions.php?$cat=<?= $category["category"] ?>">
-        //         <div><?= $category["category"];?></div>
-        //         <div class="cat-title"><?= $category["chapter"];?></div>
-        //     </div> -->
-        //     <div><?= $vragen?></div>
-        //     <?php
-        //     }
-        // }
-        ?>
 
 
 </main>
