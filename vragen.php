ca24fc3391be6173cfde7d500edd525a45c02306
TristanvanBuuren <144346103+TristanvanBuuren@users.noreply.github.com>
2024-05-31 10:57:22 +0200

    Update vragen.php

--- vragen.php
+++ vragen.php
@@ -1,7 +1,7 @@
 <?php
 include ('core/header2.php');
 
-$cat1 = $_GET['cat'];
+// $cat1 = $_GET['cat'];
 $vragen_id = isset($_GET['id']) ? $_GET['id'] : null;
 
 if ($vragen_id === null) {
@@ -36,26 +36,51 @@ if ($vragen_id === null) {
                 <div class="content-container">
                     <div class="image"><img src="assets/img/<?= $image ?>" alt="Vraag Afbeelding" class="img-fluid mx-auto d-block vraag-afbeelding2"></div>
                     <div class="vraag-box">
-                        <div class="vraag-balk2"><?= $question ?></div>
-                        <div class="antwoord-box2" onclick="selectAntwoord(this)"><?= $option_1 ?></div>
-                        <div class="antwoord-box2" onclick="selectAntwoord(this)"><?= $option_2 ?></div>
-                        <?php if($type == 1){?>
-                        <div class="antwoord-box2" onclick="selectAntwoord(this)"><?= $option_3 ?></div>
-                        <?php } ?>
+                        <!-- <form> -->
+                            <div class="vraag-balk2"><?= $question ?></div>
+                            <div class="antwoord-box2" onclick="selectAntwoord(this)"><?= $option_1 ?></div>
+                            <div class="antwoord-box2" onclick="selectAntwoord(this)"><?= $option_2 ?></div>
+                            <?php if($type == 1){?>
+                                <div class="antwoord-box2" onclick="selectAntwoord(this)"><?= $option_3 ?></div>
+                            <?php } ?>
+                        </div>
                     </div>
-                </div>
-                <div class="blauwe-balk-f">
+                    <div class="blauwe-balk-f">
                         <a href="<?php if($vragen_id == 1){echo('index.php');} elseif($vragen_id >= 2 && $vragen_id <= 20){echo('vragen.php?id=' . $vragen_id - 1);}?>">
                             <button class="terug-knop2">< Terug</button>
                         </a>
                         <div class="spacer"></div>
                         <button class="verder-knop2" onclick="verderGaan()">Verder ></button>
                     </div>
+                <!-- </form> -->
                 <?php
         }
     }
     $sqli_prepare->close();
 }
+?>
+<?php
+    // function SaveAns(){
+    //     $geselecteerdeAntwoord = document.querySelector('.antwoord-box2.selected');
+    //     if ($geselecteerdeAntwoord) {
+    //         // Haal de huidige categorie en vragen_id op uit de URL
+    //         var urlParams = new URLSearchParams(window.location.search);
+    //         var cat = urlParams.get('cat');
+    //         var id = urlParams.get('id');
+
+    //         // Verhoog de vragen_id met 1
+    //         var nieuweId = parseInt(id) + 1;
+
+    //         // Bouw de nieuwe URL op met de nieuwe vragen_id
+    //         var nieuweUrl = 'vragen.php?cat=' + cat + '&id=' + nieuweId;
+
+    //         // Navigeer naar de nieuwe URL
+    //         window.location.href = nieuweUrl;
+    //     } else {
+    //         alert("Kies eerst een antwoord voordat je verder gaat");
+    //     }
+    // }
+        
 ?>
 </main>
 <script>
